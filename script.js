474d0254d570dc7144af73c266a529ee77f2b5cc
thesharkzmanout <134110228+thesharkzmanout@users.noreply.github.com>
2023-06-13 13:54:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const changingText = document.querySelector('.changing-text');
+
+
+const texts = [
+  'Leaving is easy.. Leaving it behind you is hard.',
+  'Sharkz? SHARKZ? are you even awake? Fuck sakes.',
+  'I might hide my social security number here hehe.',
+  'Cheese balls',
+  '"life is roblox- dj kahlid',
+  '"HOW DO U KNOW I USE ARCH :0',
+  '"Drugs are rad- me 2 weeks ago',
+  '"yes" -flamingo's ban reason on roblox',
+  '"subscribe to my channel youtube.com/thesharkzmanout',
+  'I\'m quite out of context some times.',
+  'Wait shadys back?',
+  'Thank you mario but the princess is in another castle',
+  'i ding womp ping pong lollipop',
+];
+
+function changeText() {
+  const randomIndex = Math.floor(Math.random() * texts.length);
+  const newText = texts[randomIndex];
+  changingText.textContent = newText;
+}
+
+
+changeText();
+
+
+setInterval(changeText, 3000); 
